229748b6261598845b9e2913a9c908fac4750368
Tejus Sahi <tejussahi07@gmail.com>
2024-04-01 00:20:32 +0530

    Updated Name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} Project by Sahib Preet Singh</p>
+            <p>&copy; {new Date().getFullYear()} Project by Tejus</p>
         </footer>
     );
 };
